9e64781e2a4f2785578e6bc7e153f63d623f7e12
Khalid Adkins <khalidhadkins@gmail.com>
2019-02-07 19:38:04 -0700

    Webstroms added directories

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -1,4 +1,4 @@
-const axios = require('axios')
+const axios = require('axios');
 
 module.exports = async (phrase) => {
     const results = await axios.get('https://www.googleapis.com/books/v1/volumes', {
@@ -6,10 +6,22 @@ module.exports = async (phrase) => {
             format: 'json',
             q: `${phrase}`
         }
-    })
+    });
 
-    return JSON.stringify(results.data);
-}
+    //return JSON.stringify(results.data);
+    return {
+        data: results.data,
+        //this will give the bit information
+        status: results.status,
+        //this returns the status in computer form
+        statusText: results.statusText,
+        //this returns the status for people
+        headers: results.headers,
+        //this is the status for the headers
+        requestHeader: results.config.headers
+        //this is the location of the headers
+    }
+};
 
 /*  console.log(results.data);
     console.log(results.status);
@@ -23,4 +35,6 @@ module.exports = async (phrase) => {
             format: 'json',
             q: `${phrase}`,
         },
-    })*/
+    })
+
+    */
