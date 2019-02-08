6bb97d3f528abc4155906539e5d03d40f0457767
adkinskh <44444060+adkinskh@users.noreply.github.com>
2019-02-08 12:19:11 -0700

    updated so that

--- test/googlebooks.js
+++ test/googlebooks.js
@@ -11,7 +11,8 @@ describe('Test Async Books', () => {
 
     it('should return an object with list books with turing in the title', function(done) {
         getBooks('turing').then((result) => {
-            let books = JSON.parse(result);
+            let myJSONpar = JSON.parse(result);
+            let books = myJSONpar.data;
             expect(books).to.be.an('object');
             expect(books.items).to.satisfy(function(items) {
                 return items.every(function(item) {
